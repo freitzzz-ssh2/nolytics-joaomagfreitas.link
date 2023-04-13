24637de25bdf09c2ef387d1ed328e3c410ed1967
freitzzz <1160907@isep.ipp.pt>
2023-04-13 23:11:15 +0100

    Updates

--- _app/immutable/chunks/0-0c92193a.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_layout-76d3bfd6.js";import{default as t}from"../components/pages/_layout.svelte-e9370c17.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/2-88b34c4b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ca08c8b6.js";export{t as component};
